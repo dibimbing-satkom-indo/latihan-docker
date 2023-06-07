508e43224045ab721c6a9aed4378040857dd52c4
Ifan Iqbal <ifaniqbal.com@gmail.com>
2023-06-07 11:17:57 +0700

    feat: add db and router. build: add docker compose

--- contoh/main.go
+++ contoh/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	app := gin.Default()
+	dsn := "root:password@tcp(db:3306)/app?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(
+		mysql.Open(dsn),
+		&gorm.Config{},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	app.GET("/", func(context *gin.Context) {
+		version := ""
+		err := db.Raw("select version()").Scan(&version).Error
+		if err != nil {
+			log.Println(err)
+			context.Status(http.StatusInternalServerError)
+			return
+		}
+		context.String(http.StatusOK, version)
+	})
+
+	err = app.Run(":8080")
+
+	if err != nil {
+		log.Println(err)
+	}
 }
